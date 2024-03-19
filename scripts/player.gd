e5c8ab10d187ac38e15d8841bbdb9e7b97ad4ec7
EricBL3 <ericbuitron3@gmail.com>
2024-03-19 10:29:40 -0600

    Add export settings

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ class_name Player
 @onready var animator = $AnimationPlayer
 
 @export var speed = 300
+@export var accelerometer_speed = 130
 @export var stop_offset = 15
 @export var teleport_margin = 20
 @export var gravity = 15
@@ -11,8 +12,12 @@ class_name Player
 
 var screen_size
 
+var use_accelerometer = false
+
 func _ready():
 	screen_size = get_viewport_rect().size
+	if OS.get_name() == "Android" || OS.get_name() == "iOS":
+		use_accelerometer = true
 
 func _process(delta):
 	if velocity.y > 0:
@@ -28,13 +33,16 @@ func _physics_process(delta):
 	if velocity.y > max_fall_velocity:
 		velocity.y = max_fall_velocity
 	
-	var dir = Input.get_axis("move_left", "move_right")
-	if dir:
-		velocity.x = dir * speed
+	if use_accelerometer:
+		velocity.x = Input.get_accelerometer().x * accelerometer_speed
 	else:
-		# Useful for making the character slide and then stop.
-		# Change the value of stop_offset to make it stop moving slower or faster.
-		velocity.x = move_toward(velocity.x, 0, speed / stop_offset)
+		var dir = Input.get_axis("move_left", "move_right")
+		if dir:
+			velocity.x = dir * speed
+		else:
+			# Useful for making the character slide and then stop.
+			# Change the value of stop_offset to make it stop moving slower or faster.
+			velocity.x = move_toward(velocity.x, 0, speed / stop_offset)
 	
 	move_and_slide()
 	
