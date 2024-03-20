a8ad677e87434bf52761e6e910ec976c99cb5151
EricBL3 <ericbuitron3@gmail.com>
2024-03-20 17:09:09 -0600

    Complete section 4 until log messages

--- scripts/screens.gd
+++ scripts/screens.gd
@@ -1,1 +1,9 @@
 extends CanvasLayer
+@onready var console = $Debug/ConsoleLog
+
+func _ready():
+	console.visible = false
+
+
+func _on_toggle_console_pressed():
+	console.visible = !console.visible
